a86766dd1ca532f46ad73ba26c5d8b22aeb22b10
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-05-11 19:45:57 +0200

    Add support for adding todos

--- src/main.nim
+++ src/main.nim
@@ -1,16 +1,29 @@
-from htmlgen import nil
-from os import getEnv
-from strutils import parseUInt
-import asynchttpserver
-import asyncdispatch
-from xmltree import escape
+from std/base64 import nil
+from std/uri import decodeQuery
+from std/htmlgen import nil
+from std/os import getEnv
+from std/strutils import parseUInt
+import std/asynchttpserver
+import std/asyncdispatch
+from std/xmltree import escape
 
 type Todo = tuple[desc: string, done: bool]
 
 proc render_todo(todo: Todo): string =
-  htmlgen.li(escape(todo.desc))
+  let id = base64.encode(todo.desc);
+  let input =
+    if todo.done:
+        htmlgen.input(type = "checkbox", checked = "", value = id)
+      else:
+        htmlgen.input(type = "checkbox", value = id)
+  htmlgen.li(
+    htmlgen.label(
+      input,
+      escape(todo.desc)
+    )
+  )
 
-proc render_todos(todos: seq[Todo]): string =
+proc render_page(todos: seq[Todo]): string =
   var todoHtml = "";
   for todo in todos: todoHtml.add(render_todo(todo))
   "<! DOCTYPE html>" &
@@ -20,14 +33,19 @@ proc render_todos(todos: seq[Todo]): string =
     ),
     htmlgen.body(
       htmlgen.h1("todo"),
+      htmlgen.form(
+        `method` = "post",
+        htmlgen.input(type = "text", name = "desc", id = "desc"),
+        htmlgen.input(type = "submit", value = "add")
+      ),
       htmlgen.ul(todoHtml)
     )
   )
 
 proc read_todos(): seq[Todo] {.raises: [IOError].} =
   let path = getEnv("TODO_TXT_PATH")
-  let file = open(path)
-  defer: file.close()
+  let file = open(path, fmRead)
+  defer: close(file)
   var todos: seq[Todo] = @[]
   var line = ""
   while file.readLine(line):
@@ -37,6 +55,20 @@ proc read_todos(): seq[Todo] {.raises: [IOError].} =
       todos.add((desc: line, done: false))
   todos
 
+proc write_todos(todos: seq[Todo]) {.raises: [IOError].} =
+  let path = getEnv("TODO_TXT_PATH")
+  let file = open(path, fmWrite)
+  defer: close(file)
+  for todo in todos:
+    if todo.done: file.write("x ")
+    file.writeLine(todo.desc)
+
+proc add_todo(new_todo: Todo): seq[Todo] {.raises: [IOError].} =
+  var todos = read_todos()
+  if new_todo notin todos: todos.add(new_todo)
+  write_todos(todos)
+  todos
+
 proc log_error() =
   let msg = getcurrentExceptionMsg()
   echo "Got exception: ", msg
@@ -44,16 +76,27 @@ proc log_error() =
 proc handle_req(req: Request): (HttpCode, string) {.raises: [].} =
   case req.url.path:
     of "/":
-      if req.reqMethod != HttpGet:
-        (Http405, "")
-      else:
-        let todos =
-          try:
-            read_todos()
-          except:
-            log_error()
-            return (Http500, "")
-        (Http200, render_todos(todos))
+      case req.reqMethod:
+        of HttpGet:
+          let todos =
+            try: read_todos()
+            except IOError:
+              log_error()
+              return (Http500, "")
+          (Http200, render_page(todos))
+        of HttpPost:
+          for (key, value) in decodeQuery(req.body):
+            if key == "desc":
+              let todos =
+                try:
+                  add_todo((desc: value, done: false))
+                except IOError:
+                  log_error()
+                  return (Http500, "")
+              return (Http200, render_page(todos))
+          (Http400, "")
+        else:
+          (Http405, "")
     else:
       (Http404, "")
 
