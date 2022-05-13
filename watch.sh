4487fe477b165f22cf22cf6247b45f939c473826
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-05-13 17:15:53 +0200

    Don't commit test-todos.txt file
    So I don't have to clean up changes made to it after doing some testing.

--- watch.sh
+++ watch.sh
@@ -2,6 +2,10 @@
 #! nix-shell -i bash -p entr
 # shellcheck shell=bash
 
+cat <<EOT > test-todos.txt
+x do laundry
+wash dishes
+EOT
 export PORT=8080
 export TODO_TXT_PATH=test-todos.txt
 git ls-files | entr -ccr -s "nim r src/main.nim"
